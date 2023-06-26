fe12cae865d4dc58253266d061358007e6ee740c
VIPTankz <t.y.l.e.r.14@live.co.uk>
2023-06-26 13:29:10 +0100

    set SPR

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import torch as T
 
 if __name__ == '__main__':
 
-    from DrDER_Agent import Agent
-    agent_name = "DrDER"
+    from SPR_Agent import Agent
+    agent_name = "SPR"
 
     """
     games = ["Alien","Amidar","Assault","Asterix","BankHeist","BattleZone","Boxing","Breakout","ChopperCommand","CrazyClimber",\
