8460edb3b26b874cbc75408664e759af1bc4e081
VIPTankz <t.y.l.e.r.14@live.co.uk>
2023-07-13 11:01:40 +0100

    set DrQ

--- DrQ_Agent.py
+++ DrQ_Agent.py
@@ -188,17 +188,15 @@ class Agent():
 
         indices = np.arange(self.batch_size)
 
-        #states_aug = (self.intensity(self.random_shift(states.float()))).to(T.uint8)
-        #states_aug_ = (self.intensity(self.random_shift(states_.float()))).to(T.uint8)
-        #states_aug_policy_ = (self.intensity(self.random_shift(states_.float()))).to(T.uint8)
+        states_aug = (self.intensity(self.random_shift(states.float()))).to(T.uint8)
+        states_aug_ = (self.intensity(self.random_shift(states_.float()))).to(T.uint8)
+        states_aug_policy_ = (self.intensity(self.random_shift(states_.float()))).to(T.uint8)
 
-        V_s, A_s = self.net.forward(states)  # states_aug
+        V_s, A_s = self.net.forward(states_aug)  # states_aug
 
-        V_s_, A_s_ = self.tgt_net.forward(states_)  # states_aug_
+        V_s_, A_s_ = self.tgt_net.forward(states_aug_)  # states_aug_
 
-        #V_s_eval, A_s_eval = self.tgt_net.forward(states_)  # states_aug_policy_
-        V_s_eval = V_s_
-        A_s_eval = A_s_
+        V_s_eval, A_s_eval = self.tgt_net.forward(states_aug_policy_)  # states_aug_policy_
 
         q_pred = T.add(V_s, (A_s - A_s.mean(dim=1, keepdim=True)))[indices, actions]
 
